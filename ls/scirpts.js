344bf541337633565c17b4507db67964cc58c028
Aubrey Masten and Dominic Brown <dominic@>
2017-04-03 09:22:37 -0700

    initial commit with boilerplate files and code

---
+++ ls/scirpts.js
@@ -0,0 +1,16 @@
+// BACK END
+
+function Location(name, description) {
+  this.name = name;
+  this.description = description;
+}
+
+// FRONT END
+$(document).ready(function(){
+  $("form#myForm").submit(function(event){
+    event.preventDefault();
+
+
+
+  });
+});
